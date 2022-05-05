3aaeba23bf709d0cb875953b6f87484f6321cd1e
Christian Inkster <christian@hopefullyuseful.com>
2022-05-05 12:42:54 +0800

    Plugin stuff

---
+++ scripts/build-soracom/pre-built-plugins/grafana-clock-panel/signplugin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+if [[ -z "${GRAFANA_API_KEY}" ]]; then
+  echo "GRAFANA_API_KEY environment variable not defined.  Check 1Password and define before running"
+fi
+npx @grafana/toolkit plugin:sign --rootUrls https://jp.lagoon.soracom.io,https://jp-v2.lagoon.soracom.io,https://jp-v2-staging.lagoon.soracom.io,https://jp-v2-dev.lagoon.soracom.io,https://g-v2.lagoon.soracom.io,https://g-v2.lagoon.soracom.io,https://g-v2-staging.lagoon.soracom.io,http://localhost:3000
