7c84e02cc8a0c49bfbec8a58706e9b77e0c9fae4
Christian Inkster <christian@soracom.io>
2023-01-20 17:26:04 +0900

    Small fix

--- scripts/build-soracom/precheck.sh
+++ scripts/build-soracom/precheck.sh
@@ -3,4 +3,4 @@
 # make sure we are disabling alerts that were migrated from lagoon v2
 grep -Fq ALERT_DISABLED pkg/services/ngalert/store/alert_rule.go || { echo 'Source does not contain PR to DISABLE ALERTS using labels!' ; exit 1; }
 # make sure we dont show admin users in the permission lists
-grep -Fq filerAdminUsers pkg/services/accesscontrol/resourcepermissions/api.go || { echo 'Source does not contain PR to FILTER USERS in the permission list!' ; exit 1; }
+grep -Fq filterAdminUsers pkg/services/accesscontrol/resourcepermissions/api.go || { echo 'Source does not contain PR to FILTER USERS in the permission list!' ; exit 1; }
