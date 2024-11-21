acf904c3a8a7db4b7e47f609ed9ff2fc5e345c01
Shogo Okada <shogo@soracom.jp>
2024-11-21 03:23:42 +0000

    [build] Log cleanup

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -14,15 +14,24 @@ cd $PLUGIN_DIR
 
 clone_private_repo () {
   if [ ! -z "$2" ]; then
+    echo "Cloning $1 at commit $2"
     COMMIT=$2
   else
     echo "ERROR No commit specified for $1, exiting"
     exit 1
   fi
+
+  # Suppress message about initial branch name
+  # Suppress message about detached head
+  git config --global init.defaultBranch main --global advice.detachedHead false
+
   
   if [ -d $1 ]; then
     cd $1 || exit
+    echo "git fetch origin $COMMIT"
     git fetch origin $COMMIT
+
+    echo "git checkout FETCH_HEAD"
     git checkout FETCH_HEAD
     cd ..
   else
@@ -37,7 +46,7 @@ clone_private_repo () {
     echo "cd $1 || exit"
     cd $1 || exit
 
-    echo "export GIT_SSH_COMMAND=\"ssh -i ../deploy_keys/$1/id_rsa -F /dev/null\""
+    echo "export GIT_SSH_COMMAND=\"ssh -i ../../deploy_keys/$1/id_rsa -F /dev/null\""
     export GIT_SSH_COMMAND="ssh -i ../../deploy_keys/$1/id_rsa -F /dev/null"
 
     echo "git remote add origin git@github.com:soracom/$1.git"
