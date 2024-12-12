b12b2223f53557ba537ab87f30e2687f21a51e94
Shogo Okada <shogo@soracom.jp>
2024-12-12 05:45:47 +0000

    [build] Updated harvest srcrev again

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -105,7 +105,7 @@ yarn_build_repo () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 83df7e2fcd5eac6cb6a6b6b9405f937c4937928a
+clone_private_repo soracom-harvest-backend 0b4961e1a9f7727c73b4e2c1c208d734fbc92078
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
