5d29a9de552c64bb9e3df9e855376fff79993b99
Shogo Okada <shogo@soracom.jp>
2024-12-13 06:57:42 +0000

    Updated harvest backend srcrev to version with Andon fixes merged into master

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -105,7 +105,7 @@ yarn_build_repo () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 8fa36f8d721ad2a4948521c88ab42b8a47038a94
+clone_private_repo soracom-harvest-backend 321c8e2d400fcd8e32f52a770254830597bc9b57
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
