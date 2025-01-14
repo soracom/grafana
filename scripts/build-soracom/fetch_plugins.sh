c101a5e4ea5572f5b2844cbf78d6016b5d988bb9
Shogo Okada <shogo@soracom.jp>
2025-01-14 23:40:24 +0000

    [build] Rebuild with new merged harvest backend

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -105,7 +105,7 @@ yarn_build_repo () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 65c66b0ab3febbc9832e0d896fa6ade2c6a620cb
+clone_private_repo soracom-harvest-backend 397dc0d70a04b0f06233a651ecfb2a655a9de88f
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
