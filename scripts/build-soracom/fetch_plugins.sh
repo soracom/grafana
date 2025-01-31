1cd061374ffbcafa9614edd738a41727ef58c145
Shogo Okada <shogo@soracom.jp>
2025-01-31 07:49:40 +0000

    [build] Updated harvest backend srcrev for UI fixes.

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -105,7 +105,7 @@ yarn_build_repo () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 20164d56f09f646848b1288d7dceed90abce45f4
+clone_private_repo soracom-harvest-backend fce1825e1e070450098e7d00da148a7382e031b5
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
