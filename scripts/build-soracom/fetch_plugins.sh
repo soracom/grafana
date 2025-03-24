db3a5606d16f9ca9b1a6049585ba1db29c09fbd0
Shogo Okada <shogo@soracom.jp>
2025-03-24 08:12:03 +0000

    Added support for lagoon plugins not on main branch. Replaced image panel with v2

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -108,6 +108,7 @@ yarn_build_repo () {
 download_artifact_from_s3 () {
   plugin_name=$1
   version=$2
+  branch=$3
 
   if [ -z "$plugin_name" ]; then
     echo "ERROR No plugin name specified, exiting"
@@ -119,20 +120,29 @@ download_artifact_from_s3 () {
     exit 1
   fi
 
-  aws s3 cp "s3://lagoon-plugins/${plugin_name}-${version}.zip" .
+  if [ ! -z "$branch" ]; then
+    basename="${plugin_name}-${version}-${branch}"
+  else
+    basename="${plugin_name}-${version}"
+  fi
 
-  if [ -f "$plugin_name-$version.zip" ]; then
-    unzip -o "$plugin_name-$version.zip"
-    rm -f "$plugin_name-$version.zip"
+  aws s3 cp "s3://lagoon-plugins/${basename}.zip.sha1" .
+  aws s3 cp "s3://lagoon-plugins/${basename}.zip" .
+
+  if [ -f "${basename}.zip" ]; then
+    sha1sum -c "${basename}.zip.sha1"
+    unzip -o "${basename}.zip"
+    rm -f "${basename}.zip.sha1"
+    rm -f "${basename}.zip"
   fi
 }
 
 clone_private_repo soracom-harvest-backend 1ecd1455048f12efdb85e1189aa7f2eedbbfa8d1
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
-clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
 
 download_artifact_from_s3 soracom-dynamic-image-panel 2.0.0
+download_artifact_from_s3 soracom-image-panel 2.0.0 sc-134810-migrate-soracom-image-panel-to-react-from
 
 #Add any pre-built plugins to the dir
 cp -R ../pre-built-plugins/* .
