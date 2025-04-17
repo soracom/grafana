c5313805df68ffcb7cfb3eead956b67e09290af0
Shogo Okada <shogo@soracom.jp>
2025-04-17 08:55:45 +0000

    [build] Fix sc-139147

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -137,7 +137,7 @@ download_artifact_from_s3 () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 1ecd1455048f12efdb85e1189aa7f2eedbbfa8d1
+clone_private_repo soracom-harvest-backend 18678e0b9785ed33b63259ae87b265bbfa712fac
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
