ff1aebcfef2f11b5313625a42223c02561009eb9
Shogo Okada <shogo@soracom.jp>
2025-04-28 07:41:11 +0000

    [build] Updated srcrev to master branch for harvest backend

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -137,7 +137,7 @@ download_artifact_from_s3 () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend edd4aa7e5be939c4494d21489f2b5e66f11e2c1d
+clone_private_repo soracom-harvest-backend f026d7aa2dc718728b484a25682a0e517944d9dc
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-image-panel a3385ba1e6507cb8cc7efff29fe96af1b55b10f5
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
