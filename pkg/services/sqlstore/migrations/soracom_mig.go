b3d7d1eaec34b82da151fe1cf00eef6f7e593c08
Shogo Okada <shogo@soracom.jp>
2025-06-13 00:31:18 +0000

    [build] Adds line to mirate secrets namespace as well.

--- pkg/services/sqlstore/migrations/soracom_mig.go
+++ pkg/services/sqlstore/migrations/soracom_mig.go
@@ -3,6 +3,6 @@ package migrations
 import . "github.com/grafana/grafana/pkg/services/sqlstore/migrator"
 
 func addSoracomDatasourceNameMigration(mg *Migrator) {
-	const sql = `UPDATE data_source SET name = 'Soracom' WHERE type = 'harvest-backend-datasource' AND name = 'Harvest';`
-	mg.AddMigration("rename soracom datasource name", NewRawSQLMigration(sql))
+	mg.AddMigration("rename soracom datasource name", NewRawSQLMigration(`UPDATE data_source SET name = 'Soracom' WHERE type = 'harvest-backend-datasource' AND name = 'Harvest';`))
+	mg.AddMigration("rename soracom datasource secrets namespace", NewRawSQLMigration(`UPDATE secrets SET namespace = 'Soracom' WHERE type = 'datasource' AND namespace = 'Harvest';`))
 }
