d4c34841cf306862bbd2f6bda37356862304b341
Shogo Okada <shogo@soracom.jp>
2025-07-04 01:12:06 +0000

    [build] Updated harvest backend to reflect PR review.

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -137,7 +137,7 @@ download_artifact_from_s3 () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend 5407080801d7cd742c5b85c3b95297d0156119b1
+clone_private_repo soracom-harvest-backend ada76e359dcb8b3a1be0980d2e04112030498e73
 clone_private_repo soracom-map-panel 59be62df090b858cad049b64db5527d9d8c5ef05
 clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
 
