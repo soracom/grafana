52b23590b1e51e0719de5028b9981ca34e00b5a8
Shogo Okada <shogo@soracom.jp>
2025-08-13 06:47:31 +0000

    [build] with indevelopment map panel

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -143,6 +143,7 @@ clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
 
 download_artifact_from_s3 soracom-dynamic-image-panel 2.0.0
 download_artifact_from_s3 soracom-image-panel 2.0.0
+download_artifact_from_s3 soracom-map-panel 2.0.0 sc-134811-migrate-soracom-map-panel-to-react-from
 
 #Add any pre-built plugins to the dir
 cp -R ../pre-built-plugins/* .
