b7604a2a2666441f3bfbf6bdf3f88459b5de411f
Shogo Okada <shogo@soracom.jp>
2025-09-01 10:05:17 +0000

    [build] Test build with plugin versions from the last 9.3.9 build

--- scripts/build-soracom/fetch_plugins.sh
+++ scripts/build-soracom/fetch_plugins.sh
@@ -137,12 +137,12 @@ download_artifact_from_s3 () {
   fi
 }
 
-clone_private_repo soracom-harvest-backend HASH_PLACEHOLDER
-clone_private_repo soracom-map-panel HASH_PLACEHOLDER
-clone_private_repo soracom-plot-panel HASH_PLACEHOLDER
+clone_private_repo soracom-harvest-backend 8e0ca55851f74d1e4edd34f45f5168e76a04727a
+clone_private_repo soracom-plot-panel a166c5f3da64896d6ac6a2ddc39b4551dbc5c9c3
 
-download_artifact_from_s3 soracom-dynamic-image-panel VERSION_PLACEHOLDER
-download_artifact_from_s3 soracom-image-panel VERSION_PLACEHOLDER
+download_artifact_from_s3 soracom-dynamic-image-panel 2.0.0
+download_artifact_from_s3 soracom-image-panel 2.0.0
+download_artifact_from_s3 soracom-map-panel 2.0.0
 
 #Add any pre-built plugins to the dir
 cp -R ../pre-built-plugins/* .
